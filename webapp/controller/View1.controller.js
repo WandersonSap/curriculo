1c72deb0384eb030e44a6a744afcbff3d857b3a4
WandersonSap <wandersoncarvalho@gmail.com>
2020-05-20 23:43:07 +0000

    criando modelo manualmente

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ovly.data.binding.curriculo.controller.View1", {
 		onInit: function () {
+			var oFonteDeDados = {
+				nome: "Wanderson",
+				sobrenome: "Santos"
+			};
+			
+			var oModeloPessoal = new JSONModel(oFonteDeDados);
 
 		}
 	});
